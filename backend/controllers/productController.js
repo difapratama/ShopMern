a928b528f6980faf8f9542fe5199105cff79f060
difapratama <masfuddifapratama@gmail.com>
2021-04-16 22:37:45 +0700

    add searching features && update script shell bug

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/product");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const APIFeatures = require("../utils/apiFeatures");
 
 // Create new product
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
@@ -13,11 +14,13 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get all products
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find();
+  const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+
+  const products = await apiFeatures.query;
   res.status(200).json({
     success: true,
-    products,
     count: products.length,
+    products,
   });
 });
 
